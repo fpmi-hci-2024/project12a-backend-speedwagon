637a654a51d505981d1a513f8429d35784c32076
Egorishche7 <egor.gulin04@gmail.com>
2024-11-22 17:49:33 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Speed Wagon Backend
---
+++ src/test/java/by/speedteam/speedwagon/SpeedWagonApplicationTests.java
@@ -0,0 +1,13 @@
+package by.speedteam.speedwagon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedWagonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
